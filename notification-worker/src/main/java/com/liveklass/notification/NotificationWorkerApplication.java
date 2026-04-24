319f45126b1ae4ae17241012cf8e1330e57f8e23
naver0504 <qortndwls1234@gmail.com>
2026-04-24 20:20:14 +0900

    ♻️ refactor: notification worker 애플리케이션 패키지 이동
    - NotificationWorkerApplication 클래스를 com.liveklass.notification 패키지로 이동해 worker 부트 진입점 정리
    - 기존 notification.worker.NotificationWorkerApplication 파일을 제거해 중복 진입점 정리

---
+++ notification-worker/src/main/java/com/liveklass/notification/NotificationWorkerApplication.java
@@ -0,0 +1,12 @@
+package com.liveklass.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationWorkerApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(NotificationWorkerApplication.class, args);
+    }
+}
