860c2a31b40bcd50306b7d5825d89e20b04d0a66
naver0504 <qortndwls1234@gmail.com>
2026-04-24 23:59:23 +0900

    ✨ feat: NotificationException UNSUPPORTED_CHANNEL_TYPE 추가
    - UNSUPPORTED_CHANNEL_TYPE 예외를 추가해 지원하지 않는 발송 채널 요청 시 400 반환

--- notification-core/src/main/java/com/liveklass/notification/domain/exception/NotificationException.java
+++ notification-core/src/main/java/com/liveklass/notification/domain/exception/NotificationException.java
@@ -1,6 +1,7 @@
 package com.liveklass.notification.domain.exception;
 
 import com.liveklass.common.error.ExceptionInterface;
+import com.liveklass.common.error.exception.BadRequestException;
 import com.liveklass.common.error.exception.ForbiddenException;
 import com.liveklass.common.error.exception.NotFoundException;
 import lombok.AllArgsConstructor;
@@ -10,8 +11,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum NotificationException implements ExceptionInterface {
 
-    NOTIFICATION_NOT_FOUND(  "NOTIFICATION_001", "알림을 찾을 수 없습니다.",          NotFoundException.class),
-    NOTIFICATION_ACCESS_DENIED("NOTIFICATION_002", "해당 알림에 접근 권한이 없습니다.", ForbiddenException.class);
+    NOTIFICATION_NOT_FOUND(    "NOTIFICATION_001", "알림을 찾을 수 없습니다.",              NotFoundException.class),
+    NOTIFICATION_ACCESS_DENIED("NOTIFICATION_002", "해당 알림에 접근 권한이 없습니다.",     ForbiddenException.class),
+    UNSUPPORTED_CHANNEL_TYPE(  "NOTIFICATION_003", "지원하지 않는 발송 채널입니다.",        BadRequestException.class);
 
     private final String errorCode;
     private final String message;
